0e47408b5e49b53616db68015d29c7d816ddfc30
Duncan <buzzbuzz.macdonald@gmail.com>
2017-10-20 11:33:54 -0700

    even better
    lots of comments are gone

--- Herd_Immunity_Project/virus.py
+++ Herd_Immunity_Project/virus.py
@@ -1,20 +1,4 @@
 class Virus(object):
-   #  ‘’'
-   #  Virus objects will infect persons
-   #
-   # _____Attributes______:
-   #
-   # name: String: Contains virus name.
-   #
-   # infection_rate: Int. The chance that the virus spreads to a new human.
-   #
-   # kill_rate: The chance that the virus kills the host.
-   #
-   # _____Methods_____:
-   #
-   # __init__(self, name, infection_rate, kill_rate)
-   #      -All parameters should be set to the passed value.
-   #  ‘’'
 
    def __init__(self, name, infection_rate, kill_rate):
         self.name = name
